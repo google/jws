a78bbe1489d6a7c1555c11ba0d83f43e621ed6d1
Quan Nguyen <msuntmquan@gmail.com>
2018-08-22 11:57:37 -0700

    Add JwtMacAuthenticator, JwtMacVerify, JwtPublicKeySign

--- tests/jwt_test.py
+++ tests/jwt_test.py
@@ -19,7 +19,6 @@ import json
 import test_vector
 import unittest
 from jwt import jwt
-from jws import jws
 import test_util
 from jws.cleartext_jwk_set_reader import CleartextJwkSetReader
 import calendar
@@ -29,10 +28,10 @@ import datetime
 # TODO(quannguyen): add more tests.
 class JwtTest(unittest.TestCase):
 
-  def test_jwt_claims_issuer_subject_audiences(self):
+  def test_jwt_public_key_verifier_with_issuer_subject_audiences(self):
     # Sign
     priv_key = CleartextJwkSetReader.from_json(test_vector.json_rsa_priv_key)
-    signer = jws.JwsPublicKeySign(priv_key)
+    signer = jwt.JwtPublicKeySign(priv_key)
     signed_token = signer.sign(test_vector.test_header_rsa,
                                test_vector.test_payload)
 
@@ -59,10 +58,10 @@ class JwtTest(unittest.TestCase):
     verifier = jwt.JwtPublicKeyVerify(pub_key, 'issuer1', 'subject1', ['aud'])
     self.assertFalse(verifier.verify(signed_token))
 
-  def test_jwt_claims_exp_nbf(self):
+  def test_jwt_public_key_verifier_with_exp_nbf(self):
     # Sign
     priv_key = CleartextJwkSetReader.from_json(test_vector.json_rsa_priv_key)
-    signer = jws.JwsPublicKeySign(priv_key)
+    signer = jwt.JwtPublicKeySign(priv_key)
     # Valid exp time.
     payload = json.loads(test_vector.test_payload)
     payload['exp'] = _get_unix_timestamp() + 100
@@ -100,6 +99,82 @@ class JwtTest(unittest.TestCase):
     payload['nbf'] = _get_unix_timestamp() + 100
     payload = json.dumps(payload)
     signed_token = signer.sign(test_vector.test_header_rsa, payload)
+
+    # Verify
+    self.assertFalse(verifier.verify(signed_token))
+
+  def test_jwt_mac_verifier_with_issuer_subject_audiences(self):
+    # Authenticate
+    key = CleartextJwkSetReader.from_json(test_vector.json_hmac_key)
+    authenticator = jwt.JwtMacAuthenticator(key)
+    signed_token = authenticator.authenticate(test_vector.test_header_hmac,
+                                              test_vector.test_payload)
+
+    # Verify
+    # Ignore issuer, subject and audience.
+    verifier = jwt.JwtMacVerify(key)
+    self.assertTrue(verifier.verify(signed_token))
+    # Correct issuer, subject and audience.
+    verifier = jwt.JwtMacVerify(key, 'issuer1', 'subject1', ['aud1', 'aud2'])
+    self.assertTrue(verifier.verify(signed_token))
+    # Modify token.
+    for modified_token in test_util.modify_token(signed_token):
+      self.assertFalse(verifier.verify(modified_token))
+
+    # Incorrect issuer.
+    verifier = jwt.JwtMacVerify(key, 'issuer0', 'subject1', ['aud1'])
+    self.assertFalse(verifier.verify(signed_token))
+    # Incorrect subject.
+    verifier = jwt.JwtMacVerify(key, 'issuer1', 'subject0', ['aud1'])
+    self.assertFalse(verifier.verify(signed_token))
+    # Incorrect audience.
+    verifier = jwt.JwtMacVerify(key, 'issuer1', 'subject1', ['aud'])
+    self.assertFalse(verifier.verify(signed_token))
+
+  def test_jwt_public_key_verifier_with_exp_nbf(self):
+    # Authenticate
+    key = CleartextJwkSetReader.from_json(test_vector.json_hmac_key)
+    authenticator = jwt.JwtMacAuthenticator(key)
+    # Valid exp time.
+    payload = json.loads(test_vector.test_payload)
+    payload['exp'] = _get_unix_timestamp() + 100
+    payload = json.dumps(payload)
+    signed_token = authenticator.authenticate(test_vector.test_header_hmac,
+                                              payload)
+
+    # Verify
+    verifier = jwt.JwtMacVerify(key)
+    self.assertTrue(verifier.verify(signed_token))
+
+    # Invalid exp time.
+    payload = json.loads(test_vector.test_payload)
+
+    payload['exp'] = _get_unix_timestamp() - 100
+    payload = json.dumps(payload)
+    signed_token = authenticator.authenticate(test_vector.test_header_hmac,
+                                              payload)
+
+    # Verify
+    self.assertFalse(verifier.verify(signed_token))
+
+    # Valid nbf time.
+    payload = json.loads(test_vector.test_payload)
+
+    payload['nbf'] = _get_unix_timestamp() - 100
+    payload = json.dumps(payload)
+    signed_token = authenticator.authenticate(test_vector.test_header_hmac,
+                                              payload)
+
+    # Verify
+    self.assertTrue(verifier.verify(signed_token))
+
+    # Invalid nbf time.
+    payload = json.loads(test_vector.test_payload)
+
+    payload['nbf'] = _get_unix_timestamp() + 100
+    payload = json.dumps(payload)
+    signed_token = authenticator.authenticate(test_vector.test_header_hmac,
+                                              payload)
 
     # Verify
     self.assertFalse(verifier.verify(signed_token))
