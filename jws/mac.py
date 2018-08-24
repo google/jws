72a17c46cdb2585d68abf8ff30ea32e7462e5ec7
Quan Nguyen <msuntmquan@gmail.com>
2018-08-24 13:30:42 -0700

    Fix license header and doc

--- jws/mac.py
+++ jws/mac.py
@@ -1,29 +1,16 @@
-"""Copyright 2018 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.Copyright 2018 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Abstract base class for message authentication code (MAC).
 
 Security guarantees:
@@ -32,9 +19,7 @@ Message Authentication Codes provide symmetric message authentication.
 Instances implementing this abstract class are secure against existential
 forgery under chosen plaintext attack, and can be deterministic or randomized.
 This abstract class should be used for authentication only, and not for other
-purposes like generation of pseudorandom bytes.
-
-"""
+purposes like generation of pseudorandom bytes."""
 
 __author__ = "quannguyen@google.com (Quan Nguyen)"
 
